583d5634a1ef7bc170267e936c5f5c128ef2c7b9
M. Thaler <mats68@gmail.com>
2024-11-23 10:10:07 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Für Vite's HTML-Dateien
+    "./src/**/*.{js,ts,jsx,tsx}" // Frontend-Komponenten in React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
